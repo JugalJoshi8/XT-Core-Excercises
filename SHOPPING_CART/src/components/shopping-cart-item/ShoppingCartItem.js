251eed5fbdc899593f4c547eb096e4d376b5e2a2
Jugal <jkj.619@gmail.com>
2018-12-12 16:20:50 +0530

    changes

--- SHOPPING_CART/src/components/shopping-cart-item/ShoppingCartItem.js
+++ SHOPPING_CART/src/components/shopping-cart-item/ShoppingCartItem.js
@@ -6,7 +6,7 @@ export default class ShoppingCartItem {
     render() {
         const markUp = `
         <div class = 'cart-container'>
-            <div class = 'cart-item'>
+            <div class = 'cart-item cart-item--mobile'>
                 <div class = 'cart-item__img'>
                     <img src = 'imgs/${this.item.imgName}' class = 'cart-item__img' alt = 'cloth'>
                 </div>
@@ -22,7 +22,28 @@ export default class ShoppingCartItem {
                     </div>
                 </div>
             </div>
-            <div class = 'cart-item__buttons'>
+            <div class = 'cart-item cart-item--desktop'>
+                <div class = 'cart-item__img'>
+                    <img src = 'imgs/${this.item.imgName}' class = 'cart-item__img' alt = 'cloth'>
+                </div>
+                <div class = 'cart-item__desc'>
+                    <div class = 'cart-item__name' >${this.item.name}</div>
+                    <div class = 'cart-item__style'>Style #: ${this.item.styleId}</div>
+                    <div class = 'cart-item__color'>Colour: ${this.item.color}</div>
+                    <div class = 'cart-item__buttons cart-item__buttons--desktop'>
+                        <button class = 'button cart-item__button'>Edit</button>
+                        <button class = 'button cart-item__button'>Remove</button>
+                        <button class = 'button cart-item__button'>Save for Later</button>
+                    </div>
+                </div>
+                <div class = 'cart-item__size'>Size: ${this.item.size}</div>
+                <div class = 'cart-item__quantity'>QTY: <span class = 'cart-item__quantity--number'>${this.item.quantity}<span></div>
+                <div class = 'cart-item__price'>
+                    <span class = 'cart-item__currency'>${this.item.currency}</span>
+                    ${this.item.price}
+                </div>
+            </div>
+            <div class = 'cart-item__buttons cart-item__buttons--mobile'>
                 <button class = 'button cart-item__button'>Edit</button>
                 <button class = 'button cart-item__button'>Remove</button>
                 <button class = 'button cart-item__button'>Save for Later</button>
